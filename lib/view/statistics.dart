899fba547cc57b56363f49c765d3e48b78e802b9
Onur Yilmaz <onuryilmaz2015@hotmail.com>
2022-08-09 14:39:28 +0300

    code update

--- lib/view/statistics.dart
+++ lib/view/statistics.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:statistics_project/models/statistic_model.dart';
 import 'package:statistics_project/state/graph_class.dart';
 
+import '../constant/constant.dart';
 import '../controller/controller.dart';
 import '../languages/languages.dart';
 import '../state/graph.dart';
@@ -27,13 +28,14 @@ class _StatisticsAnalysisState extends State<StatisticsAnalysis> {
   String property = ProjectText().DefaultProperty;
   String property2 = ProjectText().DefaultProperty2;
   String dropDownItem = ProjectText().DefaultDropDownText;
-  String timeInterval = 'allTime';
+  String timeInterval = DefaultTimeInterval;
   
 
 
 
   Future<void> fetchStudentItem() async {
-  var response = await Dio().get("https://mocki.io/v1/be763439-3c7c-4c72-b0dd-5c3d0efd1c18");
+   var response = await Dio().get("https://mocki.io/v1/be763439-3c7c-4c72-b0dd-5c3d0efd1c18");
+  //var response = await Dio().get("https://192.168.68.106:44393/api/Mobile/Transfer/GetAllStatisticsFromMongo");
   var data = response.data.map((e) => Statistics.fromJson(e)).toList();
   for (var element in data) {
     Data.add(element);
@@ -45,7 +47,7 @@ class _StatisticsAnalysisState extends State<StatisticsAnalysis> {
     DateTime NowDate = DateTime.now();
     List<Statistics> StatisticsDataWithTimeInterval = [];
 
-    if (timeInterval == "allTime"){
+    if (timeInterval == DefaultTimeInterval){
       return Data;
     }
 
@@ -119,7 +121,7 @@ class _StatisticsAnalysisState extends State<StatisticsAnalysis> {
                 left:MediaQuery.of(context).size.width * 0.02,
                 right: MediaQuery.of(context).size.width * 0.02),
               child: Card(
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(cardBorderRadius)),
                 child: _Graph?? Text(ProjectText().GraphErrorMessage) 
                 ),
             ),
@@ -143,11 +145,11 @@ class _StatisticsAnalysisState extends State<StatisticsAnalysis> {
   List<Widget> get _TimeIntervalPopUpMenu {
     return <Widget>[PopupMenuButton(
         icon: const Icon(Icons.update_outlined),
-        iconSize: 30,
-        color: Colors.blue[50],
-        shape: const RoundedRectangleBorder(
+        iconSize: projectIconSize,
+        color: projectBlueColor,
+        shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.all(
-               Radius.circular(20.0),
+               Radius.circular(menuBorderRadius),
           ),
         ),
         itemBuilder: 
@@ -159,21 +161,23 @@ class _StatisticsAnalysisState extends State<StatisticsAnalysis> {
           PopupMenuItem(value: 'Last1Month',child: Text(TimeInterval["Last1Month"]["Name"])),
           PopupMenuItem(value: 'Last1Week',child: Text(TimeInterval["Last1Week"]["Name"]))
           ]),
-          onSelected: (String SelectedTime) {
-            setState(() {
-              timeInterval = SelectedTime;
-              StatisticsData = FilterTimeInterval(timeInterval);
-              CharDataNow = CreateChartDataNow(StatisticsData,property,property2);
-               _Graph = DrawGraphNow(CharDataNow: CharDataNow,Property: property,GraphType: 'Anlık',);
-            });
-          },)];
+          onSelected: _UpdateTimeInterval,)];
   }
+
+  void _UpdateTimeInterval(String SelectedTime) {
+          setState(() {
+            timeInterval = SelectedTime;
+            StatisticsData = FilterTimeInterval(timeInterval);
+            CharDataNow = CreateChartDataNow(StatisticsData,property,property2);
+             _Graph = DrawGraphNow(CharDataNow: CharDataNow,Property: property,GraphType: 'Anlık',);
+          });
+        }
 
 
   DropdownButton<String> DropDownList() {
     return DropdownButton(
-            borderRadius: const BorderRadius.all(Radius.circular(20.0)),
-            dropdownColor: Colors.blue[50],
+            borderRadius: BorderRadius.all(Radius.circular(menuBorderRadius)),
+            dropdownColor: projectBlueColor,
             value: dropDownItem,
             icon: const Icon(Icons.keyboard_arrow_down),   
             items: DrowDownitems.map((String items) {
@@ -181,14 +185,14 @@ class _StatisticsAnalysisState extends State<StatisticsAnalysis> {
                 value: items,
                 child: Text(items,
                 style: Theme.of(context).textTheme.subtitle1?.copyWith(
-                  color: const Color.fromARGB(255, 40, 14, 106),
+                  color: projectDarkBlueColor,
                 ),),
               );
             }).toList(),
             onChanged: (String? newValue) {
               setState(() {
                 dropDownItem = newValue!;
-                timeInterval = "allTime";
+                timeInterval = DefaultTimeInterval;
                 _changeGraphProperty(dropDownItem);
               });
               
@@ -197,12 +201,12 @@ class _StatisticsAnalysisState extends State<StatisticsAnalysis> {
   }
 
   Padding _grafphbutton(String ButtonText, String ButtonOperation) => Padding(
-    padding:  EdgeInsets.all(5.0),
+    padding:  EdgeInsets.all(buttonPadding),
     child: ElevatedButton(
       style : ElevatedButton.styleFrom(
-        primary: const Color.fromARGB(255, 16, 139, 240),
+        primary: elevatedButtonColor,
         shape:  RoundedRectangleBorder(
-          borderRadius:  BorderRadius.circular(30.0),
+          borderRadius:  BorderRadius.circular(buttonBorderRadius),
         ),
         ),
       onPressed:() => _changeGraph(ButtonOperation) , 
@@ -220,7 +224,7 @@ class _WelcomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Image.asset('assets/png/logo3.png')
+      child: Image.asset(LcwLogoPath)
       );
   }
 }
